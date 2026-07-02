106aa742bdcd2e6223222a5c524cbb163235244e
Emil Temirov <emil.temirov@gmail.com>
2026-07-02 18:43:29 +0300

    test(connections): migrate tests/sql/postgres/unit/test_data_mutations.py to evolved model

--- libs/connections/tests/sql/postgres/unit/test_data_mutations.py
+++ libs/connections/tests/sql/postgres/unit/test_data_mutations.py
@@ -99,8 +99,8 @@ def test_insert_multiple(database_connection: MockPostgresConnection) -> None:
 
     database_connection.execute_mock.assert_has_calls([
         mock.call('INSERT INTO "customers" ("id", "name") VALUES (%s, %s)', ('1', 'customer')),
-        mock.call('INSERT INTO "customers" ("age", "id", "name") VALUES (%s, %s, %s)', (25, '2', 'customer')),
-        mock.call('INSERT INTO "orders" ("amount", "customer_id", "id") VALUES (%s, %s, %s)', (100, '1', '1')),
+        mock.call('INSERT INTO "customers" ("id", "name", "age") VALUES (%s, %s, %s)', ('2', 'customer', 25)),
+        mock.call('INSERT INTO "orders" ("id", "customer_id", "amount") VALUES (%s, %s, %s)', ('1', '1', 100)),
     ])
 
 
@@ -109,8 +109,8 @@ def test_insert_multiple__with_namespaces(database_connection: MockPostgresConne
 
     database_connection.execute_mock.assert_has_calls([
         mock.call('INSERT INTO "ns1"."customers" ("id", "name") VALUES (%s, %s)', ('1', 'customer')),
-        mock.call('INSERT INTO "ns1"."customers" ("age", "id", "name") VALUES (%s, %s, %s)', (25, '2', 'customer')),
-        mock.call('INSERT INTO "ns2"."orders" ("amount", "customer_id", "id") VALUES (%s, %s, %s)', (100, '1', '1')),
+        mock.call('INSERT INTO "ns1"."customers" ("id", "name", "age") VALUES (%s, %s, %s)', ('2', 'customer', 25)),
+        mock.call('INSERT INTO "ns2"."orders" ("id", "customer_id", "amount") VALUES (%s, %s, %s)', ('1', '1', 100)),
     ])
 
 
@@ -148,7 +148,7 @@ def test_delete(database_connection: MockPostgresConnection) -> None:
     delete_customer(database_connection)
 
     database_connection.execute_mock.assert_called_once_with(
-        'DELETE FROM "customers" WHERE age < %s',
+        'DELETE FROM "customers" WHERE "age" < %s',
         (27,),
     )
 
@@ -157,7 +157,7 @@ def test_delete__with_namespace(database_connection: MockPostgresConnection) -> 
     delete_customer(database_connection, namespace='ns1')
 
     database_connection.execute_mock.assert_called_once_with(
-        'DELETE FROM "ns1"."customers" WHERE age < %s',
+        'DELETE FROM "ns1"."customers" WHERE "age" < %s',
         (27,),
     )
 
