ce6ba55d293298abc3a73eab2ea63aae0a3ff737
Emil Temirov <emil.temirov@gmail.com>
2026-07-02 18:55:17 +0300

    test(connections): rewrite postgres table_info test to use introspect_schema

--- libs/connections/tests/sql/postgres/integration/test_table_info.py
+++ libs/connections/tests/sql/postgres/integration/test_table_info.py
@@ -1,12 +1,23 @@
+from amsdal_glue_core.common.data_models.conditions import Condition
+from amsdal_glue_core.common.data_models.conditions import Conditions
 from amsdal_glue_core.common.data_models.constraints import ForeignKeyConstraint
 from amsdal_glue_core.common.data_models.constraints import PrimaryKeyConstraint
+from amsdal_glue_core.common.data_models.field_reference import Field
+from amsdal_glue_core.common.data_models.field_reference import FieldReference
+from amsdal_glue_core.common.data_models.indexes import IndexField
+from amsdal_glue_core.common.data_models.indexes import IndexSchema
+from amsdal_glue_core.common.data_models.query import QueryStatement
 from amsdal_glue_core.common.data_models.schema import PropertySchema
 from amsdal_glue_core.common.data_models.schema import Schema
 from amsdal_glue_core.common.data_models.schema import SchemaReference
+from amsdal_glue_core.common.enums import FieldLookup
 from amsdal_glue_core.common.enums import ScalarType
 from amsdal_glue_core.common.enums import Version
+from amsdal_glue_core.common.expressions.field_reference import FieldReferenceExpression
+from amsdal_glue_core.common.expressions.value import Value
 
 from amsdal_glue_connections.sql.connections.postgres_connection import PostgresConnection
+from amsdal_glue_connections.sql.schema_registry import TABLE_REGISTRY
 
 
 def test_simple_table_info(database_connection: PostgresConnection) -> None:
@@ -22,60 +33,29 @@ def test_simple_table_info(database_connection: PostgresConnection) -> None:
 
     database_connection.execute('CREATE INDEX orders_customer_id_idx ON orders (customer_id)')
 
-    assert (
-        [
-            PropertySchema(
-                name='id', type=ScalarType.SERIAL, required=True, description=None, default=None
-            ),
-            PropertySchema(name='customer_id', type=ScalarType.INTEGER, required=False, description=None, default=None),
-            PropertySchema(name='amount', type=ScalarType.INTEGER, required=False, description=None, default=None),
-            PropertySchema(name='date', type=ScalarType.DATE, required=False, description=None, default=None),
-        ],
-        [
-            PrimaryKeyConstraint(
-                name='orders_pkey',
-                fields=[
-                    'id',
-                ],
-            ),
-            ForeignKeyConstraint(
-                name='orders_customer_id_fkey',
-                fields=['customer_id'],
-                reference_schema=SchemaReference(name='customers', version=Version.LATEST, alias=None),
-                reference_fields=['id'],
-            ),
-        ],
-        [],
-    ) == database_connection.get_table_info('orders')
-
-    assert [
-        Schema(
-            name='customers',
-            version=Version.LATEST,
-            extends=None,
-            properties=[
-                PropertySchema(
-                    name='id',
-                    type=ScalarType.SERIAL,
-                    required=True,
-                    description=None,
-                    default=None,
+    assert database_connection.introspect_schema(
+        QueryStatement(
+            table=SchemaReference(name=TABLE_REGISTRY, version=Version.LATEST),
+            where=Conditions(
+                Condition(
+                    left=FieldReferenceExpression(
+                        field_reference=FieldReference(field=Field(name='name'), table_name=TABLE_REGISTRY)
+                    ),
+                    lookup=FieldLookup.EQ,
+                    right=Value('orders'),
                 ),
-                PropertySchema(name='age', type=ScalarType.INTEGER, required=False, description=None, default=None),
-                PropertySchema(name='name', type=ScalarType.TEXT, required=False, description=None, default=None),
-            ],
-            constraints=[PrimaryKeyConstraint(name='customers_pkey', fields=['id'])],
-            indexes=[],
-        ),
+            ),
+        )
+    ) == [
         Schema(
             name='orders',
             version=Version.LATEST,
-            extends=None,
+            namespace='public',
             properties=[
+                PropertySchema(name='id', type=ScalarType.SERIAL, required=True, description=None, default=None),
                 PropertySchema(
-                    name='id', type=ScalarType.SERIAL, required=True, description=None, default=None
+                    name='customer_id', type=ScalarType.INTEGER, required=False, description=None, default=None
                 ),
-                PropertySchema(name='customer_id', type=ScalarType.INTEGER, required=False, description=None, default=None),
                 PropertySchema(name='amount', type=ScalarType.INTEGER, required=False, description=None, default=None),
                 PropertySchema(name='date', type=ScalarType.DATE, required=False, description=None, default=None),
             ],
@@ -88,6 +68,56 @@ def test_simple_table_info(database_connection: PostgresConnection) -> None:
                     reference_fields=['id'],
                 ),
             ],
-            indexes=[],
+            indexes=[
+                IndexSchema(
+                    name='orders_customer_id_idx',
+                    fields=[IndexField(name='customer_id')],
+                ),
+            ],
         ),
-    ] == database_connection.query_schema()
+    ]
+
+    assert database_connection.introspect_schema(
+        QueryStatement(table=SchemaReference(name=TABLE_REGISTRY, version=Version.LATEST))
+    ) == [
+        Schema(
+            name='customers',
+            version=Version.LATEST,
+            namespace='public',
+            properties=[
+                PropertySchema(name='id', type=ScalarType.SERIAL, required=True, description=None, default=None),
+                PropertySchema(name='name', type=ScalarType.TEXT, required=False, description=None, default=None),
+                PropertySchema(name='age', type=ScalarType.INTEGER, required=False, description=None, default=None),
+            ],
+            constraints=[PrimaryKeyConstraint(name='customers_pkey', fields=['id'])],
+            indexes=None,
+        ),
+        Schema(
+            name='orders',
+            version=Version.LATEST,
+            namespace='public',
+            properties=[
+                PropertySchema(name='id', type=ScalarType.SERIAL, required=True, description=None, default=None),
+                PropertySchema(
+                    name='customer_id', type=ScalarType.INTEGER, required=False, description=None, default=None
+                ),
+                PropertySchema(name='amount', type=ScalarType.INTEGER, required=False, description=None, default=None),
+                PropertySchema(name='date', type=ScalarType.DATE, required=False, description=None, default=None),
+            ],
+            constraints=[
+                PrimaryKeyConstraint(name='orders_pkey', fields=['id']),
+                ForeignKeyConstraint(
+                    name='orders_customer_id_fkey',
+                    fields=['customer_id'],
+                    reference_schema=SchemaReference(name='customers', version=Version.LATEST, alias=None),
+                    reference_fields=['id'],
+                ),
+            ],
+            indexes=[
+                IndexSchema(
+                    name='orders_customer_id_idx',
+                    fields=[IndexField(name='customer_id')],
+                ),
+            ],
+        ),
+    ]
