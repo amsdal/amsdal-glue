b65de727e652ea4328ae808932c90e3bdd9b8aa5
Emil Temirov <emil.temirov@gmail.com>
2026-07-02 20:42:07 +0300

    test(amsdal-glue): migrate sqlite integration test_lock_service to evolved model

--- libs/amsdal-glue/tests/integration/sqlite/test_lock_service.py
+++ libs/amsdal-glue/tests/integration/sqlite/test_lock_service.py
@@ -12,7 +12,7 @@ from amsdal_glue_core.common.enums import LockParameter
 from amsdal_glue_core.common.enums import Version
 from amsdal_glue_core.common.interfaces.connection_manager import ConnectionManager
 from amsdal_glue_core.common.operations.commands import LockCommand
-from amsdal_glue_core.common.operations.commands import LockSchemaReference
+from amsdal_glue_core.common.operations.commands import LockReference
 from amsdal_glue_core.common.services.commands import LockCommandService
 from amsdal_glue_core.containers import Container
 
@@ -58,7 +58,7 @@ def test_lock_service() -> None:
             action=LockAction.ACQUIRE,
             mode=LockMode.EXCLUSIVE,
             parameter=LockParameter.SKIP_LOCKED,
-            locked_objects=[LockSchemaReference(schema=SchemaReference(name='customers', version=Version.LATEST))],
+            locked_objects=[LockReference(reference=SchemaReference(name='customers', version=Version.LATEST))],
         )
     )
     assert lock_result.success is True
