262ba279ea4f1ee460be08af87b08c39168ac6f6
Emil Temirov <emil.temirov@gmail.com>
2026-07-02 19:05:46 +0300

    test(connections): migrate aio_postgres test_data_mutations to evolved model
    - namespace='' -> namespace=None in testcase helpers
    - UpdateData(data=Data(...)) -> UpdateData(data={col: Value(...)})
    - Split heterogeneous batched InsertData in test_delete to avoid
      first-row column-set truncation

--- libs/connections/tests/sql/aio_postgres/testcases/data_mutations.py
+++ libs/connections/tests/sql/aio_postgres/testcases/data_mutations.py
@@ -16,7 +16,7 @@ from amsdal_glue_connections.sql.connections.postgres_connection import AsyncPos
 
 
 async def simple_customer_insert(
-    database_connection: AsyncPostgresConnection, namespace: str = ''
+    database_connection: AsyncPostgresConnection, namespace: str | None = None
 ) -> list[list[Data] | None]:
     return await database_connection.run_mutations([
         InsertData(
@@ -32,8 +32,8 @@ async def simple_customer_insert(
 
 async def insert_customers_and_orders(
     database_connection: AsyncPostgresConnection,
-    namespace_1: str = '',
-    namespace_2: str = '',
+    namespace_1: str | None = None,
+    namespace_2: str | None = None,
 ) -> list[list[Data] | None]:
     return await database_connection.run_mutations([
         InsertData(
@@ -64,7 +64,7 @@ async def insert_customers_and_orders(
 
 
 async def update_two_customers(
-    database_connection: AsyncPostgresConnection, namespace: str = ''
+    database_connection: AsyncPostgresConnection, namespace: str | None = None
 ) -> list[list[Data] | None]:
     return await database_connection.run_mutations([
         InsertData(
@@ -77,14 +77,15 @@ async def update_two_customers(
         ),
         UpdateData(
             schema=SchemaReference(name='customers', namespace=namespace, version=Version.LATEST),
-            data=Data(
-                data={'id': '1', 'name': 'new_customer'},
-            ),
+            data={
+                'id': Value('1'),
+                'name': Value('new_customer'),
+            },
         ),
     ])
 
 
-async def delete_customer(database_connection: AsyncPostgresConnection, namespace: str = '') -> list[list[Data] | None]:
+async def delete_customer(database_connection: AsyncPostgresConnection, namespace: str | None = None) -> list[list[Data] | None]:
     return await database_connection.run_mutations([
         DeleteData(
             schema=SchemaReference(name='customers', namespace=namespace, version=Version.LATEST),
