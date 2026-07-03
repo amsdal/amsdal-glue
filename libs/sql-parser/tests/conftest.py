ccf39ee9258b903ee836a39057a44310e1bcfee4
Emil Temirov <emil.temirov@gmail.com>
2026-07-03 16:16:03 +0300

    test(sql-parser): use real pytest-benchmark fixture (drop passthrough stub)

--- libs/sql-parser/tests/conftest.py
+++ libs/sql-parser/tests/conftest.py
@@ -1,7 +1,4 @@
 # mypy: disable-error-code="type-abstract"
-from collections.abc import Callable
-from typing import Any
-
 import pytest
 from amsdal_glue_core.containers import Container
 
@@ -12,13 +9,3 @@ from amsdal_glue_sql_parser.parsers.sqloxide_parser import SqlOxideParser
 @pytest.fixture(autouse=True)
 def _set_up_parser() -> None:
     Container.services.register(SqlParserBase, SqlOxideParser)
-
-
-@pytest.fixture()
-def benchmark() -> Callable[[Callable[[], Any]], Any]:
-    """Passthrough fixture used when pytest-benchmark is not installed."""
-
-    def _run(func: Callable[[], Any]) -> Any:
-        return func()
-
-    return _run
